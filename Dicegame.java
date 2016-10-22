3110b22970401339f858cbe8e4a3ddc2d8af2591
ShinHeaChoi <cinema0d@naver.com>
2016-10-22 16:01:36 +0900

    Add files via upload

---
+++ Dicegame.java
@@ -0,0 +1,51 @@
+class Dice {
+	private int number = 0;
+	public void roll() {   
+		number = (int) (Math.random() * 6);
+		System.out.println("Turn on " + number);
+	} 
+	public int getNum(){
+		return this.number;
+	}
+}
+
+
+public class Player {
+	private String name=null;
+	int num=0;
+	public Player(String _name) {
+		name=_name;
+	}
+	public void play() {
+		Dice d=new Dice();
+		//num=d.roll();
+		d.roll();
+		num=d.getNum();
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getNum() {
+		return this.num;
+	}
+}
+
+
+class DiceGame {
+	Player p1;
+	Player p2;
+	public void startGame() {
+		p1 = new Player("A");
+		p2 = new Player("B");
+		p1.play();
+		p2.play();
+		if (p1.getNum() > p2.getNum()) {
+			System.out.println(p1.getName() + " won");
+		}
+		else if (p1.getNum() == p2.getNum()) {
+			System.out.println(" draw");
+		}
+		else
+			System.out.println(p2.getName() + " won");
+		}
+}
